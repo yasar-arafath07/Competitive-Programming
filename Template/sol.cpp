8e2a30d03e1a5216ef9217574d82f414abdc5cb5
Yasar Arafath <97467833+yasarafath07@users.noreply.github.com>
2023-12-07 23:47:00 +0530

    Create sol.cpp

---
+++ Template/sol.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define ll long long 
+#define lli long long int
+#define ull unsigned long long
+#define endl '\n'
+
+#define debug(x) cout << #x << " : " << x << endl
+
+const int MOD = 1e9 + 7;
+const int INF = 1e18; 
+
+// Code  
+void Solve(){
+  
+}
+
+int32_t main(){
+	std :: ios :: sync_with_stdio(false);
+	std :: cin.tie(0);
+	std :: cout.tie(0);
+
+	#ifndef ONLINE_JUDGE
+  		freopen("input.txt", "r", stdin);
+    	freopen("output.txt", "w", stdout);
+	#endif
+
+	int t = 1;
+	cin>>t;
+
+	while(t--){
+		Solve();
+	}
+	
+	return 0;
+}
