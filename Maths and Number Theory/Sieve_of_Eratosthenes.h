48a93592b85a0adf667285bf312eeb3d9c3f58a9
Yasar Arafath <97467833+yasar-arafath07@users.noreply.github.com>
2023-12-20 17:22:49 +0530

    Update Sieve_of_Eratosthenes.h

--- Maths and Number Theory/Sieve_of_Eratosthenes.h
+++ Maths and Number Theory/Sieve_of_Eratosthenes.h
@@ -1,3 +1,20 @@
+const int MAXN = 1e6 + 10; // // '9 * 10^7' maximum 
+
+vector<bool> isPrime(MAXN + 1, true);
+isPrime[0] = isPrime[1] = false;
+
+for(int i=2; i*i<=MAXN; i++){
+    if(isPrime[i]){
+        for(int j=i*i; j<=MAXN; j+=i){
+            isPrime[j] = false;
+        }
+    }
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 const int MAXN = 1e6 + 10; // '9 * 10^7' maximum 
 
 vector<bool> isPrime(MAXN + 1, true); 
@@ -9,21 +26,6 @@ void Sieve(){
             for(int j=i*i; j<=MAXN; j+=i){
                 isPrime[j] = false;
             }
-        }
-    }
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-const int MAXN = 1e6 + 10;
-
-vector<bool> isPrime(MAXN + 1, true);
-isPrime[0] = isPrime[1] = false;
-
-for(int i=2; i*i<=MAXN; i++){
-    if(isPrime[i]){
-        for(int j=i*i; j<=MAXN; j+=i){
-            isPrime[j] = false;
         }
     }
 }
